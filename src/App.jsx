acaebc672173ff6c77df267a444e99bdf95c1366
Jose Deyvid <j_deyvid@live.com>
2024-10-13 22:02:43 -0300

    Recomecar app e implementar funcionalidades de inserir o nome e embaralhar a lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,57 @@
 import React from 'react'
 import './App.css'
 import { useState } from 'react'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function App() {
 
-  const navigate = useNavigate();
-  const [playersPerTeam, setPlayersPerTeam] = useState(5);
+  const [listOfPlayers, setListOfPlayers] = useState([]);
+  const [player, setPlayer] = useState("")
+  const [drawnPlayers, setDrawnPlayers] = useState([]);
 
-  const handleSubmit = () => {
-    navigate('/draw-players');
+  const handleAddPlayer = (e) => {
+    setListOfPlayers([...listOfPlayers, player])
+  }
+
+  function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+
+  const drawPlayers = () => {
+    const newList = listOfPlayers.map((player) => player);
+    shuffleArray(newList);
+    setDrawnPlayers([...newList]);
+  }
+
+  const handleTest = () => {
+    console.log(listOfPlayers)
+    console.log(drawnPlayers)
   }
 
   return (
     <div>
-      <form action="/DrawPlayers" onSubmit={handleSubmit}>
-        <label htmlFor="">Insira a quantidade de jogadores por equipe: </label>
-        <input type="number" value={playersPerTeam} onChange={(e) => setPlayersPerTeam(e.target.value)} />
-        <button type='submit'>Confirmar</button>
-      </form>
+      <div className="input-group">
+        <label htmlFor="">Insira o nome do jogador:</label>
+        <input type="text" onChange={(e) => setPlayer(e.target.value)} />
+        <button onClick={handleAddPlayer}>Adicionar</button>
+      </div>
+      <ul>
+        {listOfPlayers.map((player, i) => (
+          <li key={i}>{i+1} - {player}</li>
+        ))}
+      </ul>
+      <button onClick={drawPlayers}>Sortear times</button>
+      <ul>
+        {drawnPlayers.map((player, i) => (
+          <li key={i}>{player}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleTest}>test</button>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import DrawPlayers from './pages/DrawPlayers/index.jsx';
-
-import {
-  BrowserRouter,
-  Route,
-  Link,
-  Routes
-} from "react-router-dom";
+// import {
+//   BrowserRouter,
+//   Route,
+//   Link,
+//   Routes
+// } from "react-router-dom";
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/draw-players' element={<DrawPlayers />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
